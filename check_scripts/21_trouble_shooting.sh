d4673f87c31a64323074a40d499340924ed20b3a
iamhansko <iamhansko@kakao.com>
2024-01-18 06:29:41 +0900

    fix: Completed Script - 21_trouble_shooting

--- check_scripts/21_trouble_shooting.sh
+++ check_scripts/21_trouble_shooting.sh
@@ -5,8 +5,10 @@ read account_id
 
 echo -e "\n"
 
+docker ps -a | grep -v CONTAINER | awk '{print "docker stop " $1}' | sh -x
 docker ps -a | grep -v CONTAINER | awk '{print "docker rm " $1}' | sh -x
-docker images -a | grep -v IMAGE | awk '{print "docker rmi " $3}' | sh -x
+# docker images | grep -v IMAGE | awk '{print "docker rmi " $3}' | sh -x
+docker system prune -af
 
 echo -e "\033[32m"AWS ECR에 로그인합니다."\033[0m"
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin $account_id.dkr.ecr.ap-northeast-2.amazonaws.com
@@ -17,7 +19,7 @@ docker pull $account_id.dkr.ecr.ap-northeast-2.amazonaws.com/day3:latest
 echo -e "\n"
 
 echo -e "\033[32m"컨테이너를 실행합니다."\033[0m"
-docker run -d -p 18888:8080 < $account_id
+docker run -d -p 18888:8080 $account_id.dkr.ecr.ap-northeast-2.amazonaws.com/day3:latest
 echo -e "\n"
 
 echo -e "\033[32m"실행한 컨테이너가 구동되었는지 확인합니다."\033[0m"
@@ -25,10 +27,11 @@ docker ps
 echo -e "\n"
 
 echo -e "\033[32m"컨테이너 서버로 HTTP request를 호출합니다."\033[0m"
+sleep 5
 curl http://localhost:18888/health
 echo -e "\n"
 
-container_id=$(docker ps | grep 'day3:latest' | aws '{print $1}')
+container_id=$(docker ps | grep 'day3:latest' | awk '{print $1}')
 
 echo -e "\033[32m"컨테이너에 접속하여 결과값에 NAME='Alpine Linux'가 포함되어 있는지 확인합니다."\033[0m"
 docker exec -it $container_id cat /etc/os-release
@@ -39,5 +42,5 @@ docker exec -it $container_id python3 -V
 echo -e "\n"
 
 echo -e "\033[32m"LISTEN 상태의 8080포트를 가지고 있는지 확인합니다."\033[0m"
-netstat -an | grep 8080
+docker exec -it $container_id netstat -an | grep 8080
 echo -e "\n"
