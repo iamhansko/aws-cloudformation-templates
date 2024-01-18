4329520a29c1016c00bf9b66a07186f223b981dc
iamhansko <iamhansko@kakao.com>
2024-01-19 07:29:07 +0900

    fix: Auth Error - ec2_launch Script

--- source_code/21_automation/ec2_launch.sh
+++ source_code/21_automation/ec2_launch.sh
@@ -7,4 +7,12 @@ aws ec2 run-instances \
 	--key-name <PEM_KEY_NAME> \
 	--iam-instance-profile Name=wsi-api \
 	--security-group-ids <SECURITY_GROUP_ID> \
-	--subnet-id <SUBNET_ID>
+	--subnet-id <SUBNET_ID> \
+	--user-data '#!/bin/bash -xe
+	exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+	dnf update -y
+	dnf install ruby wget -y
+	wget https://aws-codedeploy-<AWS_REGIN>.s3.<AWS_REGION>.amazonaws.com/latest/install
+	chmod +x ./install
+	./install auto
+	service codedeploy-agent status'
