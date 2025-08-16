9590e9aae6b917af04e6545de5da05f70ea069fb
iamhansko <iamhansko@kakao.com>
2025-08-16 18:51:29 +0900

    chore: Rename Filenames

--- eks/console/addons/README.md
+++
@@ -1,7 +0,0 @@
-# EKS AddOn
-
-### Basic
-- vpc-cni
-- coredns
-- kube-proxy
-=> aws-node 데몬셋 자동 종료 후 재시작. coredns 디플로이, kube-proxy 데몬셋 변함없음 
--- eks/console/addons/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# List EKS Addons
-aws eks describe-addon-versions --query 'addons[].{name: addonName, latest_version: addonVersions[0].addonVersion, type: type}' --output table
-
